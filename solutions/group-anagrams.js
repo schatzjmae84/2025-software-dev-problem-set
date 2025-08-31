9c5814920ee9ac1ecfc15f8ce70ad8770dea0a16
schatzjmae84 <schatzjm84@gmail.com>
2025-08-31 13:59:34 -0500

    Solved Group Anagrams using a Hashmap.

---
+++ solutions/group-anagrams.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+let strs = ["act", "pots", "tops", "cat", "stop", "hat"];
+
+const groupAnagrams = (strs) => {
+
+    const res = {};
+    for (let s of strs) {
+        const count = new Array(26).fill(0);
+        for(let c of s) {
+            count[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+        }
+        const key = count.join(',');
+        if (!res[key]) {
+            res[key] = [];
+        }
+        res[key].push(s);
+    }
+    return Object.values(res);
+};
+
+console.log(groupAnagrams(strs));
